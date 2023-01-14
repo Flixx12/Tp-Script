4b9ddaddc6999ffb2d980efad20fa51e45174c05
Flixx12 <110493772+Flixx12@users.noreply.github.com>
2023-01-14 14:50:06 +0100

    Update TP.lua

--- TP.lua
+++ TP.lua
@@ -7,9 +7,9 @@ if Password == 00100200300 then
 	local name = Instance.new("TextBox")
 	local follow = Instance.new("TextButton")
 	local tp = Instance.new("TextButton")
+	local view = Instance.new("TextButton")
 	local following = false
-
-	--Properties:
+	local viewing = false
 
 	ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 
@@ -22,19 +22,18 @@ if Password == 00100200300 then
 	name.Name = "name"
 	name.Parent = main
 	name.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-	name.Position = UDim2.new(0.164037853, 0, 0.12862283, 0)
-	name.Size = UDim2.new(0, 212, 0, 50)
+	name.Position = UDim2.new(0.167192429, 0, 0.105038047, 0)
+	name.Size = UDim2.new(0, 210, 0, 50)
 	name.Font = Enum.Font.SourceSans
 	name.PlaceholderText = "Player Name"
 	name.Text = ""
 	name.TextColor3 = Color3.fromRGB(0, 0, 0)
 	name.TextSize = 27.000
-	name.TextWrapped = true
 
 	follow.Name = "follow"
 	follow.Parent = main
 	follow.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-	follow.Position = UDim2.new(0.334384859, 0, 0.421075672, 0)
+	follow.Position = UDim2.new(0.167192429, 0, 0.683962286, 0)
 	follow.Size = UDim2.new(0, 105, 0, 50)
 	follow.Font = Enum.Font.SourceSans
 	follow.Text = "Follow"
@@ -44,23 +43,35 @@ if Password == 00100200300 then
 	tp.Name = "tp"
 	tp.Parent = main
 	tp.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-	tp.Position = UDim2.new(0.334384859, 0, 0.704094648, 0)
+	tp.Position = UDim2.new(0.1671924, 0, 0.416358799, 0)
 	tp.Size = UDim2.new(0, 105, 0, 50)
 	tp.Font = Enum.Font.SourceSans
 	tp.Text = "Teleport"
 	tp.TextColor3 = Color3.fromRGB(0, 0, 0)
 	tp.TextSize = 25.000
 
+	view.Name = "view"
+	view.Parent = main
+	view.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+	view.Position = UDim2.new(0.520504713, 0, 0.683962286, 0)
+	view.Size = UDim2.new(0, 105, 0, 50)
+	view.Font = Enum.Font.SourceSans
+	view.Text = "View"
+	view.TextColor3 = Color3.fromRGB(0, 0, 0)
+	view.TextSize = 25.000
+
 	follow.MouseButton1Click:Connect(function()
 		if following == false then
 			following = true
-			follow()
+			follow.Text = "Unfollow"
+			followPlayer()
 		else
 			following = false
+			follow.Text = "Follow"
 		end
 	end)
 
-	function follow()
+	function followPlayer()
 		while following do
 			local player = game.Players.LocalPlayer
 			local target = game.Workspace:FindFirstChild(name.Text).HumanoidRootPart.CFrame
@@ -79,10 +90,21 @@ if Password == 00100200300 then
 		end
 	end)
 
+	view.MouseButton1Click:Connect(function()
+		if viewing == false then
+			viewing = true
+			game.Workspace.Camera.CameraSubject = game.Workspace:FindFirstChild(name.Text).Humanoid
+			view.Text = "Unview"
+		else
+			viewing = false
+			game.Workspace.Camera.CameraSubject = game.Workspace:FindFirstChild(game.Players.LocalPlayer.Name).Humanoid
+			view.Text = "View"
+		end
+	end)
+
 	ScreenGui.Parent = game.CoreGui
 	main.Active = true
 	main.Draggable = true
-
 else
 	print("Wrong Password!")
 end
