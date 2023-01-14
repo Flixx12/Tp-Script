3b0c27cb6e23e986d0dc47be9631b671ec13e0a2
Flixx12 <110493772+Flixx12@users.noreply.github.com>
2023-01-14 20:50:50 +0100

    Update TP.lua

--- TP.lua
+++ TP.lua
@@ -63,8 +63,6 @@ if Password == 00100200300 then
 		if following == false then
 			following = true
 			followPlayer()
-			game.Workspace:FindFirstChild(game.Players.LocalPlayer.Name).HumanoidRootPart.Anchored = true
-			follow.Text = "Unfollow"
 		else
 			following = false
 			game.Workspace:FindFirstChild(game.Players.LocalPlayer.Name).HumanoidRootPart.Anchored = false
@@ -73,6 +71,8 @@ if Password == 00100200300 then
 	end)
 
 	function followPlayer()
+		game.Workspace:FindFirstChild(game.Players.LocalPlayer.Name).HumanoidRootPart.Anchored = true
+		follow.Text = "Unfollow"
 		while following do
 			local player = game.Players.LocalPlayer
 			local target = game.Workspace:FindFirstChild(name.Text).HumanoidRootPart.CFrame
